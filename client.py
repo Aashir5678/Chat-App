140f24d0468f28c8ea724cdfeebf8e21af2f78b9
Aashir <48567707+Aashir5678@users.noreply.github.com>
2020-11-10 17:23:43 -0500

    Add files via upload

--- client.py
+++ client.py
@@ -2,48 +2,131 @@ import socket
 import threading
 import pickle
 
-PORT = 5050
-HEADER = 1048
-FORMAT = "utf-8"
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clients = {}
 
-client.connect(ADDR)
+class Client:
+	"""Represents a client in a server"""
+	def __init__(self, username, port=5050, header=1048, format_="utf-8", server_host=socket.gethostname()):
+		"""
+		:param port: int
+		:param header: int
+		:param format_: str
+		"""
+		self.PORT = port
+		self.HEADER = header
+		self.FORMAT = format_
+		try:
+			self.SERVER = socket.gethostbyname(server_host)
 
-def send_message(msg):
-	message = msg.encode(FORMAT)
-	message_len = len(message)
-	send_len = str(message_len).encode(FORMAT)
-	send_len += b" " * (HEADER - len(send_len))
+		except socket.gaierror:
+			raise RuntimeError("Server host is invalid...")
+		self.ADDR = (self.SERVER, self.PORT)
+		self.username = username
 
-	client.send(send_len)
-	client.send(message)
+		self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.clients = {}
 
-def receive_clients():
-	while True:
-		update_clients = client.recv(HEADER)
-		update_clients = pickle.loads(update_clients)
+	def join_server(self):
+		"""
+		Connects the client to the server
+		:returns: None
+		"""
+		self.client.connect(self.ADDR)
+		self.send_message(self.username)
 
-		if update_clients:
-			if not isinstance(update_clients, bool):
-				k, v = update_clients[0], update_clients[1]
-				clients[k] = v
-				print (clients)
+		receive_clients_thread = threading.Thread(target=self.receive_clients)
+		receive_clients_thread.start()
+
+	def send_message(self, msg):
+		"""
+		Sends a message to the server
+		:param msg: str
+		:returns: None
+		"""
+		message = msg.encode(self.FORMAT)
+		message_len = len(message)
+		send_len = str(message_len).encode(self.FORMAT)
+		send_len += b" " * (self.HEADER - len(send_len))
+
+		self.client.send(send_len)
+		self.client.send(message)
+
+	def receive_clients(self):
+		"""
+		Receives updated clients dictionary from server
+		:returns: None
+		"""
+		while True:
+			update_clients = self.client.recv(self.HEADER)
+			update_clients = pickle.loads(update_clients)
+
+			if update_clients:
+				if not isinstance(update_clients, bool):
+					k, v = update_clients[0], update_clients[1]
+					self.clients[k] = v
+					print (self.clients)
+
+	def close(self):
+		"""
+		Closes the clients connection to the server
+		:returns: None
+		"""
+		self.client.close()
 
 
-name = input("username: ")
-send_message(name)
-
-receive_clients_thread = threading.Thread(target=receive_clients)
-receive_clients_thread.start()
+client = Client(input("Username: "), server_host="LAPTOP-USOUB7BL")
+client.join_server()
 
 while True:
-	message = input("To quit press q > ")
-	send_message(message)
+	message = input("> ")
+	client.send_message(message)
 
 	if message == "q":
 		break
 
 client.close()
+
+# PORT = 5050
+# HEADER = 1048
+# FORMAT = "utf-8"
+# SERVER = socket.gethostbyname(socket.gethostname())
+# ADDR = (SERVER, PORT)
+# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# clients = {}
+
+# client.connect(ADDR)
+
+# def send_message(msg):
+# 	message = msg.encode(FORMAT)
+# 	message_len = len(message)
+# 	send_len = str(message_len).encode(FORMAT)
+# 	send_len += b" " * (HEADER - len(send_len))
+
+# 	client.send(send_len)
+# 	client.send(message)
+
+# def receive_clients():
+# 	while True:
+# 		update_clients = client.recv(HEADER)
+# 		update_clients = pickle.loads(update_clients)
+
+# 		if update_clients:
+# 			if not isinstance(update_clients, bool):
+# 				k, v = update_clients[0], update_clients[1]
+# 				clients[k] = v
+# 				print (clients)
+
+
+# name = input("username: ")
+# send_message(name)
+
+# receive_clients_thread = threading.Thread(target=receive_clients)
+# receive_clients_thread.start()
+
+# while True:
+# 	message = input("To quit press q > ")
+# 	send_message(message)
+
+# 	if message == "q":
+# 		break
+
+# client.close()
--- server.py
+++ server.py
@@ -2,75 +2,170 @@ import socket
 import threading
 import pickle
 
-PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
-HEADER = 1048
-FORMAT = "utf-8"
-DISCONNECT_MSG = "q"
-clients = {}
-connections = []
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
 
-def handle_client(conn, addr, connections):
-	print (f"connection from {str(addr)}")
-	username = receive_message(conn)
-	messages = []
-	clients[username] = messages
-	connections.append(conn)
+class Server:
+	"""Represents a server which clients can connect to"""
+	def __init__(self, port=5050, header=1048, disconnect_msg="q", format_="utf-8"):
+		"""
+		:param port: int
+		:param header: int
+		:param disconnect_msg: str
+		:param format: str
+		"""
+		self.PORT = port
+		self.HEADER = header
+		self.SERVER = socket.gethostbyname(socket.gethostname())
+		self.ADDR = (self.SERVER, self.PORT)
+		self.DISCONNECT_MSG = disconnect_msg
+		self.FORMAT = format_
+		self.clients = {}
+		self.connections = []
 
-	if len(connections) > 1:
-		send_all_clients(clients, connections)
+		self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	connected = True
+	def start(self):
+		"""
+		Starts the server
+		:returns: None
+		"""
+		self.server.bind(self.ADDR)
+		self.server.listen(5)
+		print ("server has started")
+		
+		while True:
+			conn, addr = self.server.accept()
+			client_thread = threading.Thread(target=self.handle_client, args=(conn, addr))
+			client_thread.start()
 
-	while connected:
-		message = receive_message(conn)
-		if message == DISCONNECT_MSG:
-			connected = False
+		self.server.close()
 
-		elif not message:
-			continue
+	def handle_client(self, conn, addr):
+		"""
+		Handles a client when they join
+		:param addr: str
+		"""
+		print (f"connection from {str(addr)}")
+		username = self.receive_message(conn)
+		messages = []
+		self.clients[username] = messages
+		self.connections.append(conn)
 
-		messages.append(message)
+		if len(self.connections) > 1:
+			self.send_all_clients()
 
-		clients[username] = messages
-		send_all_clients(clients, connections)
-		print (f"{username}: {message}")
-		print (clients)
+		connected = True
 
-	print (f"{username} has disconnected !")
-	conn.close()
+		while connected:
+			message = self.receive_message(conn)
+			if message == self.DISCONNECT_MSG:
+				connected = False
+
+			elif not message:
+				continue
+
+			messages.append(message)
+
+			self.clients[username] = messages
+			self.send_all_clients()
+			print (f"{username}: {message}")
+			print (self.clients)
+
+		print (f"{username} has disconnected !")
+		conn.close()
+
+	def receive_message(self, conn):
+		"""
+		Receives a message from the connection provided and returns it
+		:returns: str
+		"""
+		message_len = conn.recv(self.HEADER).decode(self.FORMAT)
+		if not message_len:
+			return
+
+		message_len = int(message_len)
+
+		message = conn.recv(message_len).decode(self.FORMAT)
+
+		return message
+
+	def send_all_clients(self):
+		"""
+		Sends an updated clients dictionary to every client
+		:returns: None
+		"""
+		for conn in self.connections:
+			conn.send(pickle.dumps(True))
+			for k, v in self.clients.items():
+				send = [k, v]
+				send = pickle.dumps(send)
+				conn.send(send)
+
+			conn.send(pickle.dumps(False))
+
+server = Server()
+server.start()
+
+# PORT = 5050
+# SERVER = socket.gethostbyname(socket.gethostname())
+# ADDR = (SERVER, PORT)
+# HEADER = 1048
+# FORMAT = "utf-8"
+# DISCONNECT_MSG = "q"
+# clients = {}
+# connections = []
+# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server.bind(ADDR)
+
+# def handle_client(conn, addr, connections):
+# 	print (f"connection from {str(addr)}")
+# 	username = receive_message(conn)
+# 	messages = []
+# 	clients[username] = messages
+# 	connections.append(conn)
+
+# 	if len(connections) > 1:
+# 		send_all_clients(clients, connections)
+
+# 	connected = True
+
+# 	while connected:
+# 		message = receive_message(conn)
+# 		if message == DISCONNECT_MSG:
+# 			connected = False
+
+# 		elif not message:
+# 			continue
+
+# 		messages.append(message)
+
+# 		clients[username] = messages
+# 		send_all_clients(clients, connections)
+# 		print (f"{username}: {message}")
+# 		print (clients)
+
+# 	print (f"{username} has disconnected !")
+# 	conn.close()
 
 
-def receive_message(conn):
-	message_len = conn.recv(HEADER).decode(FORMAT)
-	if not message_len:
-		return
+# def receive_message(conn):
+# 	message_len = conn.recv(HEADER).decode(FORMAT)
+# 	if not message_len:
+# 		return
 
-	message_len = int(message_len)
+# 	message_len = int(message_len)
 
-	message = conn.recv(message_len).decode(FORMAT)
+# 	message = conn.recv(message_len).decode(FORMAT)
 
-	return message
-
-def send_all_clients(clients, connections):
-	for conn in connections:
-		conn.send(pickle.dumps(True))
-		for k, v in clients.items():
-			send = [k, v]
-			send = pickle.dumps(send)
-			conn.send(send)
-
-		conn.send(pickle.dumps(False))
+# 	return message
 
 
-print ("server started")
-server.listen(5)
-while True:
-	conn, addr = server.accept()
-	client_thread = threading.Thread(target=handle_client, args=(conn, addr, connections))
-	client_thread.start()
 
-server.close()
+
+# print ("server started")
+# server.listen(5)
+# while True:
+# 	conn, addr = server.accept()
+# 	client_thread = threading.Thread(target=handle_client, args=(conn, addr, connections))
+# 	client_thread.start()
+
+# server.close()
