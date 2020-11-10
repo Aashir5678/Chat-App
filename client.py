b68af67acd9ecde95122d698df92069498f4d226
Aashir <48567707+Aashir5678@users.noreply.github.com>
2020-11-10 18:08:19 -0500

    Update client.py

--- client.py
+++ client.py
@@ -43,6 +43,10 @@ class Client:
 		:param msg: str
 		:returns: None
 		"""
+		if msg == "get":
+                    print (self.clients)
+                    return
+                
 		message = msg.encode(self.FORMAT)
 		message_len = len(message)
 		send_len = str(message_len).encode(self.FORMAT)
@@ -64,7 +68,6 @@ class Client:
 				if not isinstance(update_clients, bool):
 					k, v = update_clients[0], update_clients[1]
 					self.clients[k] = v
-					print (self.clients)
 
 	def close(self):
 		"""
@@ -74,7 +77,7 @@ class Client:
 		self.client.close()
 
 
-client = Client(input("Username: "))
+client = Client(input("Username: "), server_host='LAPTOP-USOUB7BL')
 client.join_server()
 
 while True:
@@ -83,51 +86,3 @@ while True:
 
 	if message == "q":
 		break
-
-client.close()
-
-# PORT = 5050
-# HEADER = 1048
-# FORMAT = "utf-8"
-# SERVER = socket.gethostbyname(socket.gethostname())
-# ADDR = (SERVER, PORT)
-# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# clients = {}
-
-# client.connect(ADDR)
-
-# def send_message(msg):
-# 	message = msg.encode(FORMAT)
-# 	message_len = len(message)
-# 	send_len = str(message_len).encode(FORMAT)
-# 	send_len += b" " * (HEADER - len(send_len))
-
-# 	client.send(send_len)
-# 	client.send(message)
-
-# def receive_clients():
-# 	while True:
-# 		update_clients = client.recv(HEADER)
-# 		update_clients = pickle.loads(update_clients)
-
-# 		if update_clients:
-# 			if not isinstance(update_clients, bool):
-# 				k, v = update_clients[0], update_clients[1]
-# 				clients[k] = v
-# 				print (clients)
-
-
-# name = input("username: ")
-# send_message(name)
-
-# receive_clients_thread = threading.Thread(target=receive_clients)
-# receive_clients_thread.start()
-
-# while True:
-# 	message = input("To quit press q > ")
-# 	send_message(message)
-
-# 	if message == "q":
-# 		break
-
-# client.close()
